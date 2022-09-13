4d51e6083913a76e0d1dc2fdfdc3585eb21a3179
Alex Sumoski <alexsumoski@gmail.com>
2022-09-12 22:00:27 -0400

    post requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const { dirname } = require('path');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.status(200).send('From the server');
 });
@@ -16,8 +18,26 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
-        
+        results: tours.length,
+        data: {
+            tours
+        }
     })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId}, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    });
 })
 
 const port = 3000;
