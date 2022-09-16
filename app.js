b6071102e9ffdb185436789e247b888c701d5e82
Alex Sumoski <alexsumoski@gmail.com>
2022-09-15 23:38:40 -0400

    delete request

--- app.js
+++ app.js
@@ -43,6 +43,21 @@ app.get('/api/v1/tours/:id', (req, res) => {
         }
     })
 })
+ 
+app.delete('/api/v1/tours/:id', (req, res) => {
+
+    if (req.params.id * 1 > tours.length ) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+})
 
 app.patch('/api/v1/tours/:id', (req, res) => {
 
