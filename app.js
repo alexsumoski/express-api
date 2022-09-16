fd69bc4cefc629aa9c3eba343eb111e50c7497ca
Alex Sumoski <alexsumoski@gmail.com>
2022-09-15 23:15:22 -0400

    error

--- app.js
+++ app.js
@@ -27,15 +27,15 @@ app.get('/api/v1/tours', (req, res) => {
  
 app.get('/api/v1/tours/:id', (req, res) => {
     const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
 
-    if (id > tours.length) {
+    if (!tour) {
         return res.status(404).json({
             status: 'failed',
             message: 'Invalid ID'
         })
     }
 
-    const tour = tours.find(el => el.id === id);
     res.status(200).json({
         status: 'success',
         data: {
