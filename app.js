d7bdead5968d55be7c91162a487e0b8e261ec8e4
Alex Sumoski <alexsumoski@gmail.com>
2022-09-18 19:35:45 -0400

    morgan install

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const fs = require('fs');
-const { dirname } = require('path');
 const app = express();
+const morgan = require('morgan');
 
+
+app.use(morgan('dev'))
 app.use(express.json());
 
 app.use((req, res, next)  => {
