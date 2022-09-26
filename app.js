e1239119a54f569aaa6e4e5d7d77c853c1d909d3
Alex Sumoski <alexsumoski@gmail.com>
2022-09-25 21:11:42 -0400

    cross env, environment variables

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
 const tourRouter = require('./routes/tourRoutes')
 const userRouter = require('./routes/userRoutes')
 
-app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
